785abda415e287eb3712dab4340fd09b7bc42f47
GooseNBK <gustavodanielborjas@outlook.com>
2022-11-03 00:06:26 -0300

    base

--- Src/client-app/src/app/main/apps/interfaces/interface-execution/InterfaceExecution.js
+++ Src/client-app/src/app/main/apps/interfaces/interface-execution/InterfaceExecution.js
@@ -48,94 +48,94 @@ function InterfaceExecution(props) {
         setEmployeeCodes(dataArray.join(","));
     }
     
-  return (
-    <Root header={<InterfaceExecutionHeader />} content={
-        <div className="w-full p-12 pt-16 sm:pt-24 lg:ltr:pr-0 lg:rtl:pl-0">
+    return (
+        <Root header={<InterfaceExecutionHeader />} content={
             <div className="w-full p-12 pt-16 sm:pt-24 lg:ltr:pr-0 lg:rtl:pl-0">
-                <PageCarded
-                    content={
-                    <>
-                        <Tabs
-                            value={tabValue}
-                            onChange={handleTabChange}
-                            indicatorColor="secondary"
-                            textColor="secondary"
-                            variant="scrollable"
-                            scrollButtons="auto"
-                            classes={{ root: 'w-full h-64 border-b-1' }}
-                            >
-                            <Tab className="h-64" label="Execution" />
-                            <Tab className="h-64" label="Setup" />
-                        </Tabs>
-                        <div className="p-16 sm:p-24 max-w-3x2">
-                            <div className={tabValue !== 0 ? 'hidden' : ''}>
-                                <div className="flex items-center mt-16 mb-12">
-                                    <SvgIcon size={20}>material-outline:people</SvgIcon>
-                                    <Typography className="font-semibold text-16 mx-8">Employee Codes</Typography>
-                                </div>
-                                <div className="flex -mx-3">
-                                    <TextField
-                                        className="mt-8 mb-16"
-                                        required
-                                        label="Parameters"
-                                        autoFocus
-                                        variant="outlined"
-                                        fullWidth
-                                        multiline
-                                        rows={5}
-                                        onChange={InputEmployeeCodes}
-                                        value={employeeCodes}
-                                    />
-                                    <Button className="mx-8 whitespace-nowrap" variant="contained" color="secondary">
-                                        <SvgIcon size={20}>material-outline:bolt</SvgIcon>
-                                        <span className="mx-8">RUN</span>
-                                    </Button>
-                                </div>                                
-                            </div>
-                            <div className={tabValue !== 1 ? 'hidden' : ''}>
-                                <div className="flex items-center mt-16 mb-12">
-                                    <SvgIcon size={20}>material-outline:cloud</SvgIcon>
-                                    <Typography className="font-semibold text-16 mx-8">Endpoint</Typography>
-                                </div>
-                                <div className="flex flex-col md:flex-row justify-center md:items-end my-24 xs:flex-col md:space-x-24">
-                                    <Select className="mt-8 mb-16" value={apiMethod} label="Methods">
-                                        <MenuItem value={"GET"}>GET</MenuItem>
-                                        <MenuItem value={"HEAD"}>HEAD</MenuItem>
-                                        <MenuItem value={"POST"}>POST</MenuItem>
-                                        <MenuItem value={"PUT"}>PUT</MenuItem>
-                                        <MenuItem value={"DELETE"}>DELETE</MenuItem>
-                                    </Select>
-                                    <TextField
-                                        className="mt-8 mb-16"
-                                        required
-                                        label="URL"
-                                        autoFocus
-                                        variant="outlined"
-                                        fullWidth
-                                        disabled
-                                        value={"http://localhost:5000"}
-                                    /> 
-                                    <TextField
+                <div className="w-full p-12 pt-16 sm:pt-24 lg:ltr:pr-0 lg:rtl:pl-0">
+                    <PageCarded
+                        content={
+                        <>
+                            <Tabs
+                                value={tabValue}
+                                onChange={handleTabChange}
+                                indicatorColor="secondary"
+                                textColor="secondary"
+                                variant="scrollable"
+                                scrollButtons="auto"
+                                classes={{ root: 'w-full h-64 border-b-1' }}
+                                >
+                                <Tab className="h-64" label="Execution" />
+                                <Tab className="h-64" label="Setup" />
+                            </Tabs>
+                            <div className="p-16 sm:p-24 max-w-3x2">
+                                <div className={tabValue !== 0 ? 'hidden' : ''}>
+                                    <div className="flex items-center mt-16 mb-12">
+                                        <SvgIcon size={20}>material-outline:people</SvgIcon>
+                                        <Typography className="font-semibold text-16 mx-8">Employee Codes</Typography>
+                                    </div>
+                                    <div className="flex -mx-3">
+                                        <TextField
                                             className="mt-8 mb-16"
                                             required
-                                            label="Endpoint"
+                                            label="Parameters"
+                                            autoFocus
+                                            variant="outlined"
+                                            fullWidth
+                                            multiline
+                                            rows={5}
+                                            onChange={InputEmployeeCodes}
+                                            value={employeeCodes}
+                                        />
+                                        <Button className="mx-8 whitespace-nowrap" variant="contained" color="secondary">
+                                            <SvgIcon size={20}>material-outline:bolt</SvgIcon>
+                                            <span className="mx-8">RUN</span>
+                                        </Button>
+                                    </div>                                
+                                </div>
+                                <div className={tabValue !== 1 ? 'hidden' : ''}>
+                                    <div className="flex items-center mt-16 mb-12">
+                                        <SvgIcon size={20}>material-outline:cloud</SvgIcon>
+                                        <Typography className="font-semibold text-16 mx-8">Endpoint</Typography>
+                                    </div>
+                                    <div className="flex flex-col md:flex-row justify-center md:items-end my-24 xs:flex-col md:space-x-24">
+                                        <Select className="mt-8 mb-16" value={apiMethod} label="Methods">
+                                            <MenuItem value={"GET"}>GET</MenuItem>
+                                            <MenuItem value={"HEAD"}>HEAD</MenuItem>
+                                            <MenuItem value={"POST"}>POST</MenuItem>
+                                            <MenuItem value={"PUT"}>PUT</MenuItem>
+                                            <MenuItem value={"DELETE"}>DELETE</MenuItem>
+                                        </Select>
+                                        <TextField
+                                            className="mt-8 mb-16"
+                                            required
+                                            label="URL"
                                             autoFocus
                                             variant="outlined"
                                             fullWidth
                                             disabled
-                                            value={"/SuccessFactors/OnDemand"}
-                                        />                          
+                                            value={"http://localhost:5000"}
+                                        /> 
+                                        <TextField
+                                                className="mt-8 mb-16"
+                                                required
+                                                label="Endpoint"
+                                                autoFocus
+                                                variant="outlined"
+                                                fullWidth
+                                                disabled
+                                                value={"/SuccessFactors/OnDemand"}
+                                            />                          
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </>
-                    }
-                />
+                        </>
+                        }
+                    />
+                </div>
             </div>
-        </div>
-      }
-    />
-  );
+        }
+        />
+    );
 }
 
 export default withReducer('interfaceExecution', reducer)(InterfaceExecution);
--- Src/client-app/src/app/main/apps/interfaces/interface-execution/store/interfaceSlice.js
+++ Src/client-app/src/app/main/apps/interfaces/interface-execution/store/interfaceSlice.js
@@ -1,22 +1,23 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getInterfaces = createAsyncThunk('interfaceExecution/list/getInterfaces', async () => {
+export const getInterfaces = createAsyncThunk('interfaceExecution/getInterfaces', async () => {
     const response = await axios.get('/api/InterfaceSetups');
 
     const data = await response.data;
+    console.log(data);
 
     return data;
 });
 
-  const interfaceSlice = createSlice({
-    name: 'interfaceExecution/list',
+const interfaceSlice = createSlice({
+    name: 'interfaceExecution',
     initialState: null,
     reducers: {},
     extraReducers: {
-      [getInterfaces.fulfilled]: (state, action) => action.payload,
+        [getInterfaces.fulfilled]: (state, action) => action.payload,
     },
-  });
+});
 
 export const selectInterfaces = ({ interfaceExecution }) => interfaceExecution;
 
