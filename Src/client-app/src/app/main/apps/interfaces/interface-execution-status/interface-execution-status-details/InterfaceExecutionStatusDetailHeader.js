dc23db136b13fa557d3bbb4d501bb7170805445a
GooseNBK <gustavodanielborjas@outlook.com>
2022-11-06 22:59:17 -0300

    Base

--- Src/client-app/src/app/main/apps/interfaces/interface-execution-status/interface-execution-status-details/InterfaceExecutionStatusDetailHeader.js
+++ Src/client-app/src/app/main/apps/interfaces/interface-execution-status/interface-execution-status-details/InterfaceExecutionStatusDetailHeader.js
@@ -2,16 +2,15 @@ import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { motion } from 'framer-motion';
-import { useFormContext } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import _ from '@lodash';
 import SvgIcon from 'src/baseComponents/core/SvgIcon/SvgIcon';
 
 function InterfaceExecutionStatusDetailHeader(props) {
-  const methods = useFormContext();
   const theme = useTheme();
-  const navigate = useNavigate();
+  const currentProcessExecution = useSelector((store) => store.interfaceExecution);
+  console.log(currentProcessExecution);
 
   return (
     <div className="flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-32 px-24 md:px-32">
@@ -29,14 +28,14 @@ function InterfaceExecutionStatusDetailHeader(props) {
 
         <div className="flex items-center max-w-full">
           <motion.div className="hidden sm:flex" initial={{ scale: 0 }} animate={{ scale: 1, transition: { delay: 0.3 } }}>
-            <img className="w-32 sm:w-48 rounded" src="assets/images/mele.png"/>
+            <img className="w-32 sm:w-48 rounded" src="assets/images/etc/process.png"/>
           </motion.div>
           <motion.div className="flex flex-col items-center sm:items-start min-w-0 mx-8 sm:mx-16" initial={{ x: -20 }} animate={{ x: 0, transition: { delay: 0.3 } }}>
             <Typography className="text-16 sm:text-20 truncate font-semibold">
-                Process Execution Details Name
+              {currentProcessExecution?.interface} ({currentProcessExecution?.status})
             </Typography>
             <Typography variant="caption" className="font-medium">
-              Process Execution Details Detail
+              Process Execution Details
             </Typography>
           </motion.div>
         </div>
